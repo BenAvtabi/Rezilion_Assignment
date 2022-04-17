744d9809c19b227a7a62b5d01b00d2c8d56c48d3
Ben Avtabi <ben.avtabi@gmail.com>
2022-04-17 04:38:10 +0300

    Add multi-threading

--- wiki.py
+++ wiki.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import threading
 
 RELATIVE_PATH_PATTERN = '/wiki/[\w\(\) ,]+'
 LINKED_ARTICLE_PATTERN = f'\"({RELATIVE_PATH_PATTERN})\"'
@@ -35,6 +36,23 @@ def does_have_link(article_url, expected_url):
 
 	return True if re.search(expected_url, page_response.text) else False
 
+def append_if_reverse_link(results, article_url, target_relative_path):
+	print(f"Fetching {article_url}")
+	if does_have_link(article_url, target_relative_path):
+		print(f"Found reverse link from {article_url}")
+		results.append(article_url)
+
+def find_reverse_link_multithreading(linked_articles, target_relative_path):
+	results = []
+	threads = []
+	for article_url in linked_articles:
+		thread = threading.Thread(target=append_if_reverse_link, args=(results, article_url, target_relative_path))
+		threads.append(thread)
+		thread.start()
+	for thread in threads:
+		thread.join()
+	return results
+
 if __name__ == "__main__":
 	target_url = input("Please enter a URL to an article in the English Wikipedia: ")
 	target_relative_path = get_relative_path(target_url)
@@ -42,12 +60,5 @@ if __name__ == "__main__":
 	print("Finding linked wiki articles...")
 	linked_articles = get_linked_articles_unique(target_url)
 
-	print("Iterating over linked articles...")
-	results = []
-	for index, article_url in enumerate(linked_articles):
-		print("Fetching {} ({}/{})".format(article_url, index+1, len(linked_articles)))
-		if does_have_link(article_url, target_relative_path):
-			print("Found reverse link!")
-			results.append(article_url)
-
+	results = find_reverse_link_multithreading(linked_articles, target_relative_path)
 	print(results)
