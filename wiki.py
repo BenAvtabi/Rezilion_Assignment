aa9d2c571922d2d9fcbeb7b665630d21effb5ced
Ben Avtabi <ben.avtabi@gmail.com>
2022-04-17 04:21:51 +0300

    Update patterns

--- wiki.py
+++ wiki.py
@@ -1,18 +1,17 @@
 import requests
 import re
 
-ARTICLE_RELATIVE_PATH_PATTERN = "/wiki/[^\"]+"
+RELATIVE_PATH_PATTERN = '/wiki/[\w\(\) ,]+'
+LINKED_ARTICLE_PATTERN = f'\"({RELATIVE_PATH_PATTERN})\"'
 
 def make_proper_URL(relative_url):
 	return f"https://en.wikipedia.org{relative_url}"
 
 def get_relative_path(target_url):
-	target_relative_path = re.search(ARTICLE_RELATIVE_PATH_PATTERN, target_url)
-	if target_relative_path:
-		target_relative_path = target_relative_path.group()
-	else:
+	target_relative_path = re.search(RELATIVE_PATH_PATTERN, target_url)
+	if not target_relative_path:
 		raise SystemExit("Could not extract relative path from given URL")
-	return target_relative_path
+	return target_relative_path.group()
 
 def get_linked_articles_unique(article_url):
 	try:
@@ -21,7 +20,10 @@ def get_linked_articles_unique(article_url):
 		print(f"Encountered a problem when trying to fetch {article_url}:")
 		raise SystemExit(e)
 
-	relative_urls = re.findall(ARTICLE_RELATIVE_PATH_PATTERN, response.text)
+	relative_urls = re.findall(LINKED_ARTICLE_PATTERN, response.text)
+	if not relative_urls:
+		raise SystemExit("Could not find any linked articles")
+
 	return set([make_proper_URL(relative_url) for relative_url in relative_urls])
 
 def does_have_link(article_url, expected_url):
@@ -31,22 +33,21 @@ def does_have_link(article_url, expected_url):
 		print(f"Encountered a problem when trying to fetch {article_url}:")
 		raise SystemExit(e)
 
-	return True if re.search(target_relative_path, page_response.text) else False
+	return True if re.search(expected_url, page_response.text) else False
 
-target_url = input("Please enter a URL to an article in the English Wikipedia: ")
-target_relative_path = get_relative_path(target_url)
+if __name__ == "__main__":
+	target_url = input("Please enter a URL to an article in the English Wikipedia: ")
+	target_relative_path = get_relative_path(target_url)
 
-print("Finding linked wiki articles...")
-linked_articles = get_linked_articles_unique(target_url)
-if not linked_articles:
-	raise SystemExit("Could not find any linked articles")
+	print("Finding linked wiki articles...")
+	linked_articles = get_linked_articles_unique(target_url)
 
-print("Iterating over linked articles...")
-results = []
-for index, article_url in enumerate(linked_articles):
-	print("Fetching {} ({}/{})".format(article_url, index+1, len(linked_articles)))
-	if does_have_link(article_url, target_relative_path):
-		print("Found reverse link!")
-		results.append(article_url)
+	print("Iterating over linked articles...")
+	results = []
+	for index, article_url in enumerate(linked_articles):
+		print("Fetching {} ({}/{})".format(article_url, index+1, len(linked_articles)))
+		if does_have_link(article_url, target_relative_path):
+			print("Found reverse link!")
+			results.append(article_url)
 
-print(results)
+	print(results)
