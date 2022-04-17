85e64e7dfdd34ea62b130f22ad94c1176a82f9dd
Ben Avtabi <ben.avtabi@gmail.com>
2022-04-17 03:24:25 +0300

    Add user-input and basic error handling

--- wiki.py
+++ wiki.py
@@ -1,21 +1,31 @@
 import requests
 import re
 
-results = []
+target_url = input("Please enter a URL to an article in the English Wikipedia: ")
 
 print("Fetching target article...")
-response = requests.get('https://en.wikipedia.org/wiki/Beersheba')
+try:
+	response = requests.get(target_url)#'https://en.wikipedia.org/wiki/Beersheba')
+except requests.exceptions.RequestException as e:
+	print(f"Encountered a problem when trying to fetch {target_url}:")
+	raise SystemExit(e)
 
 print("Finding linked wiki articles...")
-relative_urls = set(re.findall("/wiki/[a-zA-Z0-9_]+", response.content.decode()))
+relative_urls = set(re.findall("/wiki/[a-zA-Z0-9_]+", response.text))
 
 print("Iterating over linked articles...")
+results = []
 for index, relative_url in enumerate(relative_urls):
 	full_url = f"https://en.wikipedia.org{relative_url}"
 	print("Fetching {} ({}/{})".format(full_url, index+1, len(relative_urls)))
-	page_response = requests.get(full_url)
 
-	if re.search("/wiki/Beersheba", page_response.content.decode()):
+	try:
+		page_response = requests.get(full_url)
+	except requests.exceptions.RequestException as e:
+		print(f"Encountered a problem when trying to fetch {full_url}:")
+		raise SystemExit(e)
+
+	if re.search("/wiki/Beersheba", page_response.text):
 		print("Found reverse link!")
 		results.append(full_url)
 
