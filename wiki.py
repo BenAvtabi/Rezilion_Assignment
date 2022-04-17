f8aa873df1f8ef2b3a66df23f54bfeaf9a931aa1
Ben Avtabi <ben.avtabi@gmail.com>
2022-04-17 03:13:36 +0300

    Add informative output to keep me sane

--- wiki.py
+++ wiki.py
@@ -3,14 +3,20 @@ import re
 
 results = []
 
+print("Fetching target article...")
 response = requests.get('https://en.wikipedia.org/wiki/Beersheba')
-pages = re.findall("(/wiki/[a-zA-Z0-9_]+)", response.content.decode())
-pages = set(pages)
 
-for page in pages:
-	url = "https://en.wikipedia.org" + page
-	page_response = requests.get(url)
-	if re.search("(/wiki/Beersheba)", page_response.content.decode()):
-		results.append(url)
-		print("Found!")
+print("Finding linked wiki articles...")
+relative_urls = set(re.findall("/wiki/[a-zA-Z0-9_]+", response.content.decode()))
+
+print("Iterating over linked articles...")
+for index, relative_url in enumerate(relative_urls):
+	full_url = f"https://en.wikipedia.org{relative_url}"
+	print("Fetching {} ({}/{})".format(full_url, index+1, len(relative_urls)))
+	page_response = requests.get(full_url)
+
+	if re.search("/wiki/Beersheba", page_response.content.decode()):
+		print("Found reverse link!")
+		results.append(full_url)
+
 print(results)
